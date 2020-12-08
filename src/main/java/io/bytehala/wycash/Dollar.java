5ad70908fade5717f220110a28c8d4f25b5a27a0
Lem <lemuel.dulfo@gmail.com>
2020-12-07 23:57:35 -0700

    11. Deleted subclasses and obsolete tests

--- src/main/java/io/bytehala/wycash/Dollar.java
+++
@@ -1,9 +0,0 @@
-package io.bytehala.wycash;
-
-public class Dollar extends Money {
-
-    public Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-
-}
--- src/test/java/io/bytehala/wycash/MoneyTest.java
+++ src/test/java/io/bytehala/wycash/MoneyTest.java
@@ -26,15 +26,7 @@ public class MoneyTest {
         // Equality via triangulation
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
-
-    @Test
-    public void testDiffClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-        assertTrue(new Money(10, "USD").equals(new Dollar(10, "USD")));
     }
 
     @Test
